c7b2b9285ad427c481ad927dac97d1bb16ea5ed4
dream-topdev <tonistuckey8@gmail.com>
2025-03-12 02:32:42 -0400

    lint & build

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,7 +6,7 @@ if (!MONGODB_URI) {
   throw new Error("MONGODB_URI is not defined");
 }
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+const cached = (global as any).mongoose || { conn: null, promise: null };
 
 export async function connectToDatabase() {
   if (cached.conn) return cached.conn;
